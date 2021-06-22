d03521389b0637dd165cb06d0c01111b692b5e6c
jasba24 <johanbuitrago342@gmail.com>
2021-06-22 07:38:04 -0500

    feat: :fire: add header

---
+++ src/components/PfHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="header">
+    <h1>Johan Buitrago</h1>
+    <div>
+      <a v-for="(l, i) in links" :key="i" class="header-link" :href="l.href">{{
+        l.name
+      }}</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          name: "About",
+          href: "/about"
+        },
+        {
+          name: "Blog",
+          href: "/blog"
+        },
+        {
+          name: "Projects",
+          href: "/projects"
+        },
+        {
+          name: "Contact",
+          href: "#about"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+  box-shadow: 2px 6px 7px 0 grey;
+}
+
+.header-link {
+  color: inherit;
+  text-decoration: none;
+  font-size: 1.6rem;
+  margin-left: 10px;
+  font-weight: bold;
+}
+</style>
