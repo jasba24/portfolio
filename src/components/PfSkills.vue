6ba8954ef4756017db43b3259d84c4bb75ca30b0
jasba24 <johanbuitrago342@gmail.com>
2021-06-22 09:41:46 -0500

    feat: :fire: add skills

---
+++ src/components/PfSkills.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="skills">
+    <h1>Skills</h1>
+    <ul class="skills-list">
+      <li v-for="(s, i) in skills" :key="i">{{ s }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+
+  data() {
+    return {
+      skills: [
+        "HTML",
+        "CSS",
+        "Stylus",
+        "Tailwind",
+        "Bulma",
+        "JavaScript",
+        "Vue",
+        "React",
+        "APIs REST",
+        "Git y GitHub"
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.skills {
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+}
+.skills h1 {
+  margin-bottom: 20px;
+}
+.skills-list {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-columns: repeat(5, 1fr);
+  font-size: 2rem;
+  margin-bottom: 20px;
+  gap: 10px;
+}
+.skills-list li {
+  list-style: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <Hero></Hero>
+    <Skills></Skills>
   </div>
 </template>
 
 <script>
 import Hero from "@/components/PfHero";
+import Skills from '@/components/PfSkills'
 
 export default {
   name: "Home",
-  components: { Hero }
+  components: { Hero, Skills }
 };
 </script>
