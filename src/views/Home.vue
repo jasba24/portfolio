cf43ec3cd5f2d13ca86d3e9129eb704c25f37eaa
jasba24 <johanbuitrago342@gmail.com>
2021-06-22 10:55:16 -0500

    feat: :fire: show Project component at home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,52 @@
   <div class="home">
     <Hero></Hero>
     <Skills></Skills>
+    <h1>Projects</h1>
+    <section class="projects">
+      <div class="projects-container">
+        <Project
+          v-for="(p, i) in projects"
+          :logo="p.logo"
+          :icon="p.icon"
+          :href="p.href"
+        ></Project>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import logo from "@/assets/logo.png";
+import icon from "@/assets/logo.png";
 import Hero from "@/components/PfHero";
-import Skills from '@/components/PfSkills'
+import Skills from "@/components/PfSkills";
+import Project from "@/components/PfProject";
 
 export default {
   name: "Home",
-  components: { Hero, Skills }
+  components: { Hero, Skills, Project },
+
+  data() {
+    return {
+      projects: [
+        {
+          logo: logo,
+          icon: icon,
+          href: "Platzi-badges"
+        },
+        {
+          logo: logo,
+          icon: icon,
+          href: "Platzi-rooms"
+        },
+        {
+          logo: logo,
+          icon: icon,
+          href: "Platzi-badges"
+        }
+      ]
+    };
+  }
 };
 </script>
+
