7f9baecf8f550e5ce93ce2fad8258eca2db3e596
jasba24 <johanbuitrago342@gmail.com>
2021-06-29 13:09:32 -0500

    add logic to change the footer

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,6 +9,14 @@ import Profile from "@/components/PfProfile";
 
 export default {
   name: "About",
-  components: { Profile }
+  components: { Profile },
 };
 </script>
+
+<style>
+.footer.bottom {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
